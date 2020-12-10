f460a59194d12b293b5eb4cf0675abe2aa245acd
Cristhiano Mello <cristhiano.f.mello@gmail.com>
2020-12-10 17:57:35 -0300

    Formulário com campos diferentes por tipo de usuário. Faltam alguns campos

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,22 +1,48 @@
 class RegistrationsController < Devise::RegistrationsController
-  def new
-    @user = User.new
-    @user.build_expert
-  end
-
   def create
-    # if :profile == "Auxiliar externo"
-    @user = current_user.expert.build(sign_up_params)
-    if @user.save
-      redirect_to root_path
-    else
-      render :new
+    @user = User.new(sign_up_params)
+    if @user.profile == 'Auxiliar externo'
+      if @user.save && create_expert(@user) && @expert.save
+        redirect_to new_user_session_path
+      else
+        render :new
+      end
+    elsif @user.profile == "Magistrado" || @user.profile == "Servidor"
+      if @user.save && create_servant(@user) && @servant.save
+        redirect_to new_user_session_path
+      else
+        render :new
+      end
     end
   end
 
   private
 
   def sign_up_params
-    params.require(:user).permit(:full_name, :profile, :email, :password, :password_confirmation, experts_attributes: %i[doc_number phone_number city distance accept curriculum])
+    params.require(:user).permit(:full_name,
+                                 :profile,
+                                 :email,
+                                 :password,
+                                 :password_confirmation,
+                                 experts_attributes: %i[doc_number phone_number city distance accept curriculum],
+                                 servants_attributes: %i[court city])
+  end
+
+  def create_expert(user)
+    @expert = Expert.new
+    @expert.doc_number = params[:user][:experts][:doc_number]
+    @expert.phone_number = params[:user][:experts][:phone_number]
+    @expert.city = params[:user][:experts][:city]
+    @expert.distance = params[:user][:experts][:distance]
+    @expert.accept = params[:user][:experts][:accept]
+    @expert.curriculum = params[:user][:experts][:curriculum]
+    @expert.user = user
+  end
+
+  def create_servant(user)
+    @servant = Servant.new
+    @servant.court = params[:user][:servants][:court]
+    @servant.city = params[:user][:servants][:city]
+    @servant.user = user
   end
 end
---
+++ app/javascript/component/user_registration.js
@@ -0,0 +1,19 @@
+const registerUser = () => {
+  let user = document.getElementById("user_profile");
+  const expertForm = document.getElementById("form_experts");
+  const servantForm = document.getElementById("form_servants");
+  user.addEventListener("change", e => {
+    if (e.currentTarget.value !== "") {
+      if (e.currentTarget.value === "Auxiliar externo") {
+        expertForm.classList.remove("hide");
+        servantForm.classList.add("hide");
+      }
+      else {
+        servantForm.classList.remove("hide");
+        expertForm.classList.add("hide");
+      }
+    }
+  });
+};
+
+export {registerUser};
