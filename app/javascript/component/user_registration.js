b090e5229b56c16d7fb84a50b210ecc0a41a79e1
Adonay Veiga <adonayveiga@gmail.com>
2020-12-14 14:30:07 -0300

    Fake search with uggly select2

--- app/javascript/component/user_registration.js
+++ app/javascript/component/user_registration.js
@@ -2,22 +2,24 @@ const registerUser = () => {
   let user = document.getElementById("user_profile");
   const expertForm = document.getElementById("form_experts");
   const servantForm = document.getElementById("form_servants");
-  user.addEventListener("change", e => {
-    if (e.currentTarget.value !== "") {
-      if (e.currentTarget.value === "1") {
-        expertForm.classList.remove("hide");
-        servantForm.classList.add("hide");
+  if (user) {
+    user.addEventListener("change", e => {
+      if (e.currentTarget.value !== "") {
+        if (e.currentTarget.value === "1") {
+          expertForm.classList.remove("hide");
+          servantForm.classList.add("hide");
+        }
+        else {
+          servantForm.classList.remove("hide");
+          expertForm.classList.add("hide");
+        }
       }
       else {
-        servantForm.classList.remove("hide");
+        servantForm.classList.add("hide");
         expertForm.classList.add("hide");
       }
-    }
-    else {
-      servantForm.classList.add("hide");
-      expertForm.classList.add("hide");
-    }
-  });
+    });
+  }
 };
 
 export {registerUser};
--- app/models/expert.rb
+++ app/models/expert.rb
@@ -2,4 +2,14 @@ class Expert < ApplicationRecord
   belongs_to :user
   has_many :fields
   has_many :investigations
+
+  include PgSearch::Model
+  pg_search_scope :smart_search,
+                  against: [:city],
+                  associated_against: {
+                    fields: [:title, :area]
+                  },
+                  using: {
+                    tsearch: { prefix: true }
+                  }
 end
