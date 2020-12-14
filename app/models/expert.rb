e7fe046cd0280eb70514380e34fd319ff2f6aaf1
Adonay Veiga <adonayveiga@gmail.com>
2020-12-14 15:11:37 -0300

    Melhorias na busca

--- app/models/expert.rb
+++ app/models/expert.rb
@@ -4,11 +4,18 @@ class Expert < ApplicationRecord
   has_many :investigations
 
   include PgSearch::Model
-  pg_search_scope :smart_search,
+  pg_search_scope :city_search,
                   against: [:city],
                   # associated_against: {
                   #   fields: [:title, :area]
                   # },
+                  using: {
+                    tsearch: { prefix: true }
+                  }
+  pg_search_scope :pratictioner_search,
+                  associated_against: {
+                    fields: [:title, :area]
+                  },
                   using: {
                     tsearch: { prefix: true }
                   }
