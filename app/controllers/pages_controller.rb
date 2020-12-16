8bb76cf43f7d78ababdf6a5a79e88d122e283e0d
milena valle <milenavalle@gmail.com>
2020-12-16 13:47:59 -0400

    navbar-a-missao

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :about, :guidance]
 
   def home
+  end
+  def about
+  end
+  def guidance
   end
 end
