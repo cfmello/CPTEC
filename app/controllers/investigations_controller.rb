f5e893bdf6a5c6c8c88452cbded3eb720ad0a272
Adonay Veiga <adonayveiga@gmail.com>
2020-12-17 10:06:30 -0300

    Lista de convocações com verificação de acesso

--- app/controllers/investigations_controller.rb
+++ app/controllers/investigations_controller.rb
@@ -1,6 +1,6 @@
 class InvestigationsController < ApplicationController
-  before_action :validar_acesso
-  before_action :fetch_expert, except: :show
+  before_action :validar_acesso, except: :index
+  before_action :fetch_expert, except: %i[show index]
   def new
     @investigation = Investigation.new
   end
@@ -25,10 +25,30 @@ class InvestigationsController < ApplicationController
     @investigation = Investigation.find(params[:id])
   end
 
+  def index
+    @investigations = ('2'..'3').to_a.include?(current_user.profile) ? Investigation.all : Investigation.where(expert: current_user.expert)
+  end
+
+  def update
+    raise
+    @investigation = Investigation.find(params[:id])
+    if @investigation.update_attributes(investigation_params_update)
+      flash[:success] = "Opção registrada"
+      redirect_to @investigation
+    else
+      flash[:error] = @investigation.errors.messages.values.join('<br>').html_safe
+      render 'edit'
+    end
+  end
+
   private
 
   def investigation_params
     params.require(:investigation).permit(:proc_number, :call_date)
+  end
+
+  def investigation_params_update
+    params.require(:investigation).permit(:status)
   end
 
   def validar_acesso
